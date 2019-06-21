3a5be32f82bfb8a112e59efd84a577043bbfa607
Laura <laureen.pelaez.isaza@gmail.com>
2019-06-21 13:25:32 +0200

    added front view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="hero is-primary is-fullheight">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item">
+              <img
+                src="../assets/pokemon.png"
+                alt="Logo"
+              >
+            </a>
+            <span
+              class="navbar-burger burger"
+              data-target="navbarMenuHeroA"
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div
+            id="navbarMenuHeroA"
+            class="navbar-menu"
+          >
+            <div class="navbar-end">
+              <a class="navbar-item is-active">
+                Home
+              </a>
+              <router-link
+                class="navbar-item"
+                to="/documentation"
+              >Documentation</router-link>
+
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <!-- Hero content: will be in the middle -->
+    <div class="">
+      <div class="container has-text-centered">
+        <SearchBar />
+      </div>
+    </div>
+
+    <!-- Hero footer: will stick at the bottom -->
+    <div class="hero-foot has-text-centered">
+      <nav class="tabs">
+        <div class="container">
+          <p>Made by laureenpe</p>
+        </div>
+      </nav>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SearchBar from ".././components/SearchBar";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    SearchBar
   }
-}
+};
 </script>
