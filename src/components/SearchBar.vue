c1203dc6d457dd9fb2f64a3f3a2f7d86d4c4c6da
Laura <laureen.pelaez.isaza@gmail.com>
2019-06-21 13:25:12 +0200

    added searchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,79 @@
+<template>
+  <section>
+    <div class="columns is-mobile">
+      <div class="column is-half is-offset-one-quarter">
+        <h1 class="content"><b>Select your favorite:</b> {{ selected }}</h1>
+        <b-field label="Find a pokemon">
+          <b-autocomplete
+            :data="data"
+            placeholder="e.g. Fight Club"
+            field="title"
+            :loading="isFetching"
+            @typing="getAsyncData"
+            @select="option => selected = option"
+          >
+
+            <template slot-scope="props">
+              <div class="media">
+                <div class="media-left">
+                  <img
+                    width="32"
+                    :src="`https://image.tmdb.org/t/p/w500/${props.option.poster_path}`"
+                  >
+                </div>
+                <div class="media-content">
+                  {{ props.option.title }}
+                  <br>
+                  <small>
+                    Released at {{ props.option.release_date }},
+                    rated <b>{{ props.option.vote_average }}</b>
+                  </small>
+                </div>
+              </div>
+            </template>
+          </b-autocomplete>
+        </b-field>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import debounce from "lodash/debounce";
+
+export default {
+  data() {
+    return {
+      data: [],
+      selected: null,
+      isFetching: false
+    };
+  },
+  methods: {
+    // You have to install and import debounce to use it,
+    // it's not mandatory though.
+    getAsyncData: debounce(function(name) {
+      if (!name.length) {
+        this.data = [];
+        return;
+      }
+      this.isFetching = true;
+      this.$http
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=bb6f51bef07465653c3e553d6ab161a8&query=${name}`
+        )
+        .then(({ data }) => {
+          this.data = [];
+          data.results.forEach(item => this.data.push(item));
+        })
+        .catch(error => {
+          this.data = [];
+          throw error;
+        })
+        .finally(() => {
+          this.isFetching = false;
+        });
+    }, 500)
+  }
+};
+</script>
