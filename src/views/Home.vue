da9f5063ba0fc3c3797d7d8546363432722d48cd
Laura <laureen.pelaez.isaza@gmail.com>
2019-06-27 16:02:55 +0200

    added pokemon description

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -43,32 +43,28 @@
         <div class="columns is-multiline is-1-mobile">
           <div
             class="column pa-4"
-            v-for="item in data"
+            v-for="item in pokemons"
             :key="item.id"
           >
+
             <div class="card">
               <div class="additional">
                 <div class="user-card">
                   <div class="points">
-                    {{ item.name }}
+                    {{item.name}}
                   </div>
                   <figure class="image is-128x128 is-centered">
                     <img
-                      class="is-rounded"
                       :src="item.sprites.back_default"
+                      class="is-rounded"
                     >
                   </figure>
 
                 </div>
                 <div class="more-info">
-                  <h1> {{ item.name }}</h1>
+                  <h1> {{item.name}}</h1>
                   <div class="coords">
-                    <span>Group Name</span>
-                    <span>Joined January 2019</span>
-                  </div>
-                  <div class="coords">
-                    <span>Position/Role</span>
-                    <span>City, Country</span>
+                    <p>Description: {{item.description}}</p>
                   </div>
 
                 </div>
@@ -91,36 +87,52 @@
 </template>
 
 <script>
-import SearchBar from ".././components/SearchBar";
+import SearchBar from "../components/SearchBar";
 //import Card from ".././components/Cards";
 import axios from "axios";
-let urlApi = "https://pokeapi.co/api/v2/pokemon/";
+import Axios from "axios";
+import { log } from "util";
+import { async } from "q";
+
+var pokemonApi = "https://pokeapi.co/api/v2/pokemon/";
+var pokemonCharacteristics = "https://pokeapi.co/api/v2/characteristic/";
 
 export default {
-  name: "home",
   components: {
     SearchBar
-    //Card
   },
   data() {
     return {
-      data: []
+      pokemons: []
     };
   },
   methods: {
-    async getPokemonById(id) {
-      let pokemon = await axios.get(urlApi + id);
-      return pokemon;
+    async getPokemonsByName(name) {
+      let getName = await axios.get(pokemonApi + name);
+      return getName;
     }
   },
   async mounted() {
-    let pokemons = await axios.get(urlApi);
-    console.log(pokemons.data);
+    let pokemons = await axios.get(pokemonApi);
     let results = pokemons.data.results;
+
+    //iterate in results
     for (let index = 0; index < results.length; index++) {
       const element = results[index];
-      const pokemon = await this.getPokemonById(element.name);
-      this.data.push(pokemon.data);
+      const result = await this.getPokemonsByName(element.name);
+      let pokemon = result.data;
+
+      //Url Id pokemon
+      const pokemonId = pokemon.id;
+      const urlPokemon = await axios.get(pokemonCharacteristics + pokemonId);
+      const getData = urlPokemon.data;
+
+      const getDescriptions = getData.descriptions;
+      console.log("getDescription ", getDescriptions);
+
+      pokemon.description = getDescriptions[0].description;
+      this.pokemons.push(pokemon);
+      //console.log("array", this.description);
     }
   }
 };
