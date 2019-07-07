9e57fef95e02605142ba6634dd5364db54925730
Laura <laureen.pelaez.isaza@gmail.com>
2019-07-07 13:50:55 +0200

    deploy

---
+++ js/about.3201acbd.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"8bae":function(e,n,t){"use strict";t.r(n);var l=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("This will be available soon...")])])}],i=t("2877"),r={},s=Object(i["a"])(r,l,a,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=about.3201acbd.js.map
